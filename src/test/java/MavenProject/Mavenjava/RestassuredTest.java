3cf1f33ec022bb1c96e7e8d6573bb37e89a8dd4b
VISHAL <gandhivishal1432@gmail.com>
2021-11-15 17:29:32 +0530

    usa guy done this

--- src/test/java/MavenProject/Mavenjava/RestassuredTest.java
+++ src/test/java/MavenProject/Mavenjava/RestassuredTest.java
@@ -9,6 +9,14 @@ public class RestassuredTest {
 		System.out.println("postjira");
 		System.out.println("postjira2");
 		System.out.println("postjira3");
+		System.out.println("postjira4");
+		System.out.println("postjira5");
+		System.out.println("postjira6");
+		System.out.println("postjira7");
+		System.out.println("postjira8");
+		System.out.println("postjira9");
+		System.out.println("postjira10");
+		
 	}
 	
 	
